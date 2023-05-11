d9270a14c722c0350abd8aefd8aa1b580d0852a8
rakesh9651 <maurya151999@gmail.com>
2023-05-11 08:09:16 +0530

    initial commit

---
+++ src/main/java/com/ecommerce/CloudGateway/controller/FallBackController.java
@@ -0,0 +1,23 @@
+//package com.ecommerce.CloudGateway.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class FallBackController {
+//
+//    @GetMapping("/orderServiceFallBack")
+//    public String orderServiceFallBack(){
+//        return "Order service is down ";
+//    }
+//
+//    @GetMapping("/paymentServiceFallBack")
+//    public String paymentServiceFallBack(){
+//        return "Payment service is down ";
+//    }
+//
+//    @GetMapping("/productServiceFallBack")
+//    public String productServiceFallBack(){
+//        return "Product service is down ";
+//    }
+//}
---
+++ src/main/java/com/ecommerce/CloudGateway/model/AuthenticationResponse.java
@@ -0,0 +1,22 @@
+package com.ecommerce.CloudGateway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationResponse {
+
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+    private long expireAt;
+    private Collection<String> authorityList;
+}
